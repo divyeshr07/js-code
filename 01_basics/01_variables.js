1768bc1a5f72e4cd505ce69d62354b78cfb977aa
divyeshr07 <divyesh.rushkar@gmail.com>
2025-01-23 11:52:37 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 211007
+let accountEmail = "divyesh@gmail.com"
+var accountPassword = 10203
+let accountState;
+// accountCity = pune
+
+// accountId = 2
+accountEmail = "ramsita@gmail.com"
+accountPassword = 154348
+// accountCity = pune
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword])
