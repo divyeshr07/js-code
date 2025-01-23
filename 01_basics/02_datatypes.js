7fda1a8e7bad93bb7d23e5732c0af92cdfa31872
divyeshr07 <divyesh.rushkar@gmail.com>
2025-01-23 12:10:23 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(50+58)
+
+console.log("divyesh")
+
+
+let name="divyesh"
+let age=15
+let isloggedin =false
+let state;
+
+// datatypes
+//name=>2 to power 53
+// bigint
+//strting => ""
+//boolean => true/fales
+// null => 
+// undefined =>
+//symbol => unique
+
+//object
+
+console.log (typeof null)
