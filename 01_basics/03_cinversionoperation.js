1af0d391b9c98318c7761091cf8e8d00b5d65673
divyeshr07 <divyesh.rushkar@gmail.com>
2025-01-24 10:17:14 +0000

    cinversion operation

---
+++ 01_basics/03_cinversionoperation.js
@@ -0,0 +1,40 @@
+// let score=true 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let ValueInNumber =Number(score)
+// console.log=(typeof ValueInNumber);
+// console.log=(ValueInNumber)
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// let isloggedin=1
+
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+// console.log(typeof booleanisloggedin)
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+let srt1 ="divyesh"
+let srt2 ="nakum"
+
+let srt3 = srt1 + srt2
+// console.log(srt3)
+
+
+// console.log("1"+2)
+// console.log("2"+1)
+
+// console.log("1"+2+2)
+// console.log("1"+2+1)
