67320bc841bae933f1f7cc9e91a64788279d0f3e
divyeshr07 <divyesh.rushkar@gmail.com>
2025-01-27 07:27:13 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,13 +2,14 @@ const accountId = 211007
 let accountEmail = "divyesh@gmail.com"
 var accountPassword = 10203
 let accountState;
-// accountCity = pune
+accountcitiy = "surat"
 
-// accountId = 2
+// accountId = 2 //not allowd
 accountEmail = "ramsita@gmail.com"
-accountPassword = 154348
-// accountCity = pune
+accountPassword = "154348"
+accountcitiy = "amreli"
+
 
 console.log(accountId)
 
-console.table([accountId,accountEmail,accountPassword])
+console.table([accountId,accountEmail,accountPassword,accountcitiy])
